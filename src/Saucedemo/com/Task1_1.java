23fe7c28fee20ab1700f9a67ab072f1e9ae4f1ce
sharifzodah <sharifzodah@gmail.com>
2022-11-24 00:25:12 -0500

    saucedemo.com cart validation

--- src/Saucedemo/com/Task1_1.java
+++ src/Saucedemo/com/Task1_1.java
@@ -44,9 +44,30 @@ public class Task1_1 {
         driver.findElement(By.id("last-name")).sendKeys("Smith");
         driver.findElement(By.id("postal-code")).sendKeys("07650");
         driver.findElement(By.id("continue")).click();
-        int item1_price = Integer.parseInt(driver.findElement(By.xpath("//div[text() = '29.99']")).getText().substring(1));
-        int item2_price = Integer.parseInt(driver.findElement(By.xpath("//div[text() = '15.99']")).getText().substring(1));
+        double item1_price = Double.parseDouble(driver.findElement(By.xpath("//*[@id= 'item_4_title_link']/following::div[2]/child::div"))
+                .getText().substring(1));
+        double item2_price = Double.parseDouble(driver.findElement(By.xpath("//*[@id= 'item_3_title_link']/following::div[2]/child::div"))
+                .getText().substring(1));
+        double item3_price = Double.parseDouble(driver.findElement(By.xpath("//*[@id= 'item_1_title_link']/following::div[2]/child::div"))
+                .getText().substring(1));
+        double subtotal = Double.parseDouble(driver.findElement(By.className("summary_subtotal_label"))
+                .getText().replaceAll("[^\\d.]", ""));
+        double tax_amount = Double.parseDouble(driver.findElement(By.className("summary_tax_label"))
+                .getText().replaceAll("[^\\d.]", ""));
+        double total_amount = Double.parseDouble(driver.findElement(By.className("summary_total_label"))
+                .getText().replaceAll("[^\\d.]", ""));
 
+        Assert.assertEquals(item1_price, 29.99);
+        Assert.assertEquals(item2_price, 15.99);
+        Assert.assertEquals(item3_price, 15.99);
+        Assert.assertEquals(tax_amount, 4.96);
+        Assert.assertEquals(subtotal, (item1_price+item2_price+item3_price), 0.0001);
+        Assert.assertEquals(total_amount, (subtotal + tax_amount), 0.0001);
+
+        driver.findElement(By.id("finish")).click();
+
+        String complete_msg = driver.findElement(By.xpath("//*[contains(text(), 'Complete!')]")).getText();
+        Assert.assertTrue(complete_msg.contains("Complete".toUpperCase()));
 
     }
 }
